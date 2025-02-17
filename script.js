ca2312e1d501ea482fb60f9337b1ea95553b6742
natalie <wong.lt.natalie@gmail.com>
2025-02-17 10:43:36 -0500

    Fixed password field bug

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const confirmPasswordField = document.getElementById("confirm-password");
 passwordField.setCustomValidity("This field is required.");
 confirmPasswordField.setCustomValidity("This field is required.");
 
+passwordField.addEventListener("keyup", () => {
+    passwordField.value === "" 
+        ? passwordField.setCustomValidity("This field is required")
+        : passwordField.setCustomValidity("");
+});
+
 confirmPasswordField.addEventListener("keyup", () => {
     passwordField.value === confirmPasswordField.value
         ? confirmPasswordField.setCustomValidity("")
